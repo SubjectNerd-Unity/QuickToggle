74261ec59b479880c9f73c6aa74cbe27b5a16b63
Jeiel Aranal <jeiel@bunnyhug.games>
2020-05-26 13:00:21 +0800

    Remove unused reference

--- Editor/QuickToggle.cs
+++ Editor/QuickToggle.cs
@@ -19,7 +19,6 @@ THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 using System;
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 ﻿using UnityEditor;
 using UnityEngine;
